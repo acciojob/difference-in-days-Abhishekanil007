f21134cf45145c787ceade2d05578a7acc8a9304
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-20 12:51:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+	 dt1 = new Date(date1);
+	 dt2 = new Date(date2);
+
+	if(dt1 === dt2)
+	{
+		return 0;
+	}
+
+	return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24) );
+	
 };
 
-// Do not change the code below.
+// Do not change the code below. 
 const dateOne = prompt("Enter Start Date.");
 const dateTwo = prompt("Enter End Date.");
 alert(dateDiffInDays(dateOne, dateTwo));
